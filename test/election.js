fd546b9dab7a6e924da319d9605ec0c40c015267
za <m-zubairahmed@protonmail.com>
2020-10-20 03:22:02 +0530

    tests change

--- test/election.js
+++ test/election.js
@@ -3,19 +3,36 @@ var Election = artifacts.require("./Election.sol");
 contract("Election", function (accounts) {
   let electionInstance;
 
-  it("stores 2 election candidate", async () => {
+  before(async () => {
     electionInstance = await Election.deployed();
-    const numberOfCandidatesInElection = await electionInstance.numberOfCandidates();
-
-    assert.equal(numberOfCandidatesInElection, 2);
   });
 
-  it("saved candidates with correct candidate details", async () => {
-    electionInstance = await Election.deployed();
+  it("stores 3 candidate", async () => {
+    const numberOfCandidatesInElection = await electionInstance.numberOfCandidates();
 
+    assert.equal(numberOfCandidatesInElection, 3);
+  });
+
+  it("saves candidates with correct details", async () => {
     const electionCandidatesData = [
-      {candidateID: 1, candidateName: "KCR", voteCount: 0},
-      {candidateID: 2, candidateName: "YRS", voteCount: 0},
+      {
+        candidateID: 1,
+        candidateName: "Cedric Diggory",
+        candidateSchool: "Hogwarts School",
+        voteCount: 0,
+      },
+      {
+        candidateID: 2,
+        candidateName: "Fleur Delacour",
+        candidateSchool: "Beauxbatons Academy of Magic",
+        voteCount: 0,
+      },
+      {
+        candidateID: 3,
+        candidateName: "Viktor Krum",
+        candidateSchool: "Durmstrang Institute",
+        voteCount: 0,
+      },
     ];
 
     for (const candidate of electionCandidatesData) {
@@ -25,7 +42,8 @@ contract("Election", function (accounts) {
 
       assert.equal(candidate.candidateID, candidateInElection[0]);
       assert.equal(candidate.candidateName, candidateInElection[1]);
-      assert.equal(candidate.voteCount, candidateInElection[2]);
+      assert.equal(candidate.candidateSchool, candidateInElection[2]);
+      assert.equal(candidate.voteCount, candidateInElection[3]);
     }
   });
 });
